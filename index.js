c21d0df81d1a779bc33e1a628d50ce5b60b9b890
Miguel Andrade <jmandrade0@gmail.com>
2016-12-28 13:12:31 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-pointer-events-polyfill'
+  name: 'ember-pointer-events-polyfill',
+
+  included: function(appOrAddon) {
+    var app = appOrAddon.app || appOrAddon;
+    if (!app.__emberPointerEventsPolyfillIncludedInvoked) {
+      app.__emberPointerEventsPolyfillIncludedInvoked = true;
+      this._super.included.apply(this, arguments);
+
+      app.import('vendor/pep/pep.js', {
+        prepend: true
+      });
+    }
+  },
+
+  treeForVendor: function() {
+    var pepPath = path.dirname(require.resolve('pepjs'));
+    return new Funnel(this.treeGenerator(pepPath), {
+      srcDir: '/',
+      destDir: 'pep'
+    });
+  },
 };
